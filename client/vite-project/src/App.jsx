93b026e8a447954c646d4cdfb3fdcd5fb1b7de96
Gowtham <gn86923@gmail.com>
2024-10-18 23:25:51 -0700

    Done Navigation in App function

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,5 +1,18 @@
-export default function main(){
-  return <div>
-    <h1>Hi there</h1>
-  </div>
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Signup from './components/Signup'
+import Signin from './components/Signin'
+import Todos from './components/Todos'
+
+ function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signup/>} />
+        <Route path="/Signin" element={<Signin/>} />
+        <Route path="/todos" elemnet={<Todos/>}/>
+      </Routes>
+    </Router>
+  );
 }
+
+export default App
